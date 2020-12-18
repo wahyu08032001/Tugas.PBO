b83b592ab809460895a5d6e54d891aac35f6db8a
wahyu08032001 <73293739+wahyu08032001@users.noreply.github.com>
2020-12-18 13:47:10 +0700

    Update Bilangan.java

--- Bilangan.java
+++ Bilangan.java
@@ -1,2 +1,19 @@
-public class Bilangan {
+package bilangan;
+import java.util.Scanner;
+
+public class ganjilGenap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int bil;
+        System.out.println("Masukan Bilangan : ");
+        bil=input.nextInt();
+
+        if (bil%2 ==0){
+            System.out.println("Bilangan  " + bil + "  adalah Bilangan Genap");
+        }else {
+            System.out.println("Bilangan  " + bil + "  adalah bilangan Ganjil");
+        }
+
+
+    }
 }
