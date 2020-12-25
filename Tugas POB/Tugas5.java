63cae9db95e7100425be3c8b04adbbfe12553ade
wahyu08032001 <73293739+wahyu08032001@users.noreply.github.com>
2020-12-25 22:49:04 +0700

    Add files via upload

---
+++ Tugas POB/Tugas5.java
@@ -0,0 +1,10 @@
+public class Tugas4 {
+    public static void main(string[] args){
+
+        for (int x=1; x<=5; x++){
+            system.out.print(b);
+
+            system.out.println();
+        }
+    }
+}
